408559ea4ce8d748a3e197972e0602525894dd04
Imanuel David Perel <imanuel.p222@gmail.com>
2024-08-30 16:29:58 +0300

    added MyCsUtil0, and updated Board to have the ability to check how many there by key function

---
+++ my cs library 0/Games/BoardGames/Direction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsLibrary0.Games.BoardGames;
+
+/// <summary>
+/// represents all kind of two-dimantional direction of up,left,down,right, and all four diagonals
+/// </summary>
+public class Direction
+{
+    static public Direction Up=new Direction(0,1);
+    static public Direction Down=new Direction(0,-1);
+    static public Direction Right=new Direction(1,0);
+    static public Direction Left=new Direction(-1,0);
+    static public Direction UpRight=new Direction(1,1);
+    static public Direction UpLeft=new Direction(-1,1);
+    static public Direction DownRight=new Direction(1,-1);
+    static public Direction DownLeft=new Direction(-1,-1);
+    static public Direction[] FourDirection = [ Up, UpRight, Right, DownRight];
+    static public Direction[] EightDirection = [ Up, UpRight, Right, DownRight,Down,DownLeft,Left,UpLeft];
+    
+    private int x;
+    public int X { get => x; set { x = Math.Sign(value); } }
+
+    private int y;
+    public int Y { get => y; set { y = Math.Sign(value); } }
+    public Direction(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Overload for the unary minus operator
+    public static Direction operator -(Direction dir)
+    {
+        return new Direction(-dir.X, -dir.Y);
+    }
+    public override string ToString()
+    {
+        return $"{nameof(X)}={X},{nameof(Y)}={Y}";
+    }
+
+}
---
+++ my cs library 0/Primitives/MyCsUtil0.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsLibrary0.Primitives;
+
+public static class MyCsUtil0 
+{
+    static public bool EqualOrBothNull(object? value1, object? value2)
+    {
+        if (value1 == value2)
+            return true;
+        if (value1 == null || value2 == null)
+            //since if they are both null we already returned true
+            return false;
+        return value1.Equals(value2);
+    }
+}
